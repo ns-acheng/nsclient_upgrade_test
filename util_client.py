aeea5629cc6394997a891d6b9f63bf7496f2c49d
Austin Cheng <acheng@netskope.com>
2026-04-11 12:43:12 +0800

    update files

--- util_client.py
+++ util_client.py
@@ -14,7 +14,7 @@ import shutil
 import subprocess
 import sys
 import time
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from pathlib import Path
 from typing import Any, Optional
